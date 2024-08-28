90076408865575232dd211f7e29739d16c543949
LasanthaPrasad <ee_prasad@yahoo.com>
2024-08-29 01:29:13 +0530

    first commit

--- config.py
+++ config.py
@@ -5,6 +5,14 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
+    
+    # Get the DATABASE_URL from environment variables
+    DATABASE_URL = os.environ.get('DATABASE_URL')
+    
+    # If DATABASE_URL is set, replace 'postgres://' with 'postgresql://'
+    if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+    
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL or 'sqlite:///app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     GOOGLE_MAPS_API_KEY = os.environ.get('GOOGLE_MAPS_API_KEY')
