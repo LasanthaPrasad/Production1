0e810e98b1ea5cba17fd3e3ede9bee4ed56073bc
LasanthaPrasad <ee_prasad@yahoo.com>
2024-09-05 01:50:36 +0530

    first commit

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
-#from app import app
+from app import app
 
 #if __name__ == '__main__':
 #    app.run(debug=True)
 
-from app import create_app
+#from app import create_app
 
-app = create_app()
+#app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
