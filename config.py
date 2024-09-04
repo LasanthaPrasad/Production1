caa2e72d55e038634b9d13c9ff90edadd7707c06
LasanthaPrasad <ee_prasad@yahoo.com>
2024-09-04 14:46:54 +0530

    first commit

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
-load_dotenv()
+#load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or '11541514185144'
