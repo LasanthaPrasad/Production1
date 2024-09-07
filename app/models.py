e70dea2c1039ca6d9d3845b870e98b6061f496e9
LasanthaPrasad <ee_prasad@yahoo.com>
2024-09-08 01:55:00 +0530

    with Authentication1

--- app/models.py
+++ app/models.py
@@ -8,24 +8,35 @@ from flask_security import UserMixin, RoleMixin
 
 #db = SQLAlchemy()
 
-roles_users = db.Table('roles_users',
-    db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
-    db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
-)
+import uuid
+
 
 class Role(db.Model, RoleMixin):
-    id = db.Column(db.Integer(), primary_key=True)
+    __tablename__ = 'role'
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True)
     description = db.Column(db.String(255))
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255))
     active = db.Column(db.Boolean())
-    confirmed_at = db.Column(db.DateTime())
-    roles = db.relationship('Role', secondary=roles_users,
+    confirmed_at = db.Column(db.DateTime, default=datetime.utcnow)
+    fs_uniquifier = db.Column(db.String(255), unique=True, nullable=False)
+    roles = db.relationship('Role', secondary='roles_users',
                             backref=db.backref('users', lazy='dynamic'))
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.fs_uniquifier is None:
+            self.fs_uniquifier = str(uuid.uuid4())
+
+roles_users = db.Table('roles_users',
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
+    db.Column('role_id', db.Integer, db.ForeignKey('role.id'), primary_key=True)
+)
 
 
 class IrradiationForecast(db.Model):
