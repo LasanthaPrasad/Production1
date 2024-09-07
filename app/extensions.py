9bc9c0001f51cfb9840343484ee3a02894c12941
LasanthaPrasad <ee_prasad@yahoo.com>
2024-09-07 22:50:08 +0530

    with Authentication

--- app/extensions.py
+++ app/extensions.py
@@ -1,5 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager  # Note the capital 'L' and 'M'
+#from flask_login import LoginManager  # Note the capital 'L' and 'M'
+from flask_login.login_manager import LoginManager
+
+
 
 db = SQLAlchemy()
 login_manager = LoginManager()  # This should be lowercase
