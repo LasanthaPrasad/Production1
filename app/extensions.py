e9230f16cefe2b05e653df6218d6fea1e4379ec3
LasanthaPrasad <ee_prasad@yahoo.com>
2024-09-07 23:07:21 +0530

    with Authentication

--- app/extensions.py
+++ app/extensions.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-#from flask_login import LoginManager  # Note the capital 'L' and 'M'
-from flask_login.login_manager import LoginManager
+from flask_login import LoginManager  # Note the capital 'L' and 'M'
+#from flask_login.login_manager import LoginManager
 
 
 
