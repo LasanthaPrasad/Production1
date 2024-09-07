5cffaf0bd853361f7ae47f7bcd8c523b52592fb0
LasanthaPrasad <ee_prasad@yahoo.com>
2024-09-08 01:06:29 +0530

    with Authentication1

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from apscheduler.schedulers.background import BackgroundScheduler
+from flask_security import Security, SQLAlchemyUserDatastore
+from .models import db, User, Role
+
+
+
+
+
 
 db = SQLAlchemy()
 scheduler = BackgroundScheduler()
@@ -11,10 +18,29 @@ def create_app():
     
     db.init_app(app)
     
+    # Setup Flask-Security
+    user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+    security = Security(app, user_datastore)
+
+
+
     with app.app_context():
         from . import models
         db.create_all()  # Create tables if they don't exist
         
+        # Create roles
+        user_role = user_datastore.find_or_create_role(name='user', description='Regular user')
+        admin_role = user_datastore.find_or_create_role(name='admin', description='Administrator')
+        
+        # Create users
+        if not user_datastore.get_user('praasad@geoclipz.com'):
+            user_datastore.create_user(email='praasad@geoclipz.com', password='admin', roles=[admin_role])
+        if not user_datastore.get_user('ee.prasad@gmail.com'):
+            user_datastore.create_user(email='ee.prasad@gmail.com', password='userq', roles=[user_role])
+        
+
+
+
         from . import solcast_api
         
         # Fetch forecasts on startup
