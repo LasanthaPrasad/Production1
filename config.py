a685522bb5f60217889f2a2c947e1a4baa85544f
LasanthaPrasad <ee_prasad@yahoo.com>
2024-09-10 18:35:25 +0530

    with VS_forcast2

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://') or 'sqlite:///solar_forecast.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SOLCAST_API_KEY = os.environ.get('SOLCAST_API_KEY') or 'your-solcast-api-key'
+    VISUAL_CROSSING_API_KEY = os.environ.get('VISUAL_CROSSING_API_KEY')
     GOOGLE_MAPS_API_KEY = os.environ.get('GOOGLE_MAPS_API_KEY')
 
 
