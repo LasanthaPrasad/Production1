04c7fb0f72356f9147c258b8aaabfecab09bd4ac
LasanthaPrasad <ee_prasad@yahoo.com>
2024-09-10 18:22:32 +0530

    with VS_forcast2

--- clock.py
+++ clock.py
@@ -1,10 +1,23 @@
+#from apscheduler.schedulers.blocking import BlockingScheduler
+#from app.solcast_api import fetch_solcast_data
+
+#sched = BlockingScheduler()
+
+#@sched.scheduled_job('cron', hour='6-18')
+#def scheduled_job():
+#    fetch_solcast_data()
+
+#sched.start()
+
+
 from apscheduler.schedulers.blocking import BlockingScheduler
-from app.solcast_api import fetch_solcast_data
+from app.forecast_service import ForecastService
 
 sched = BlockingScheduler()
+forecast_service = ForecastService()
 
 @sched.scheduled_job('cron', hour='6-18')
 def scheduled_job():
-    fetch_solcast_data()
+    forecast_service.update_forecasts()
 
 sched.start()
