1c00eed4fbbc4b9487f4245e32bb8d6deb372bcb
LasanthaPrasad <ee_prasad@yahoo.com>
2024-09-13 01:24:28 +0530

    with VS_forcast2

--- app/models.py
+++ app/models.py
@@ -49,6 +49,7 @@ roles_users = db.Table('roles_users',
 )
  """
 
+
 class IrradiationForecast(db.Model):
     __tablename__ = 'irradiation_forecasts'
     id = db.Column(db.Integer, primary_key=True)
